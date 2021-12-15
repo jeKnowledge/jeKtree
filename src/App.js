4f231fead5e190b226a48491f34aa23c251decfa
Vasques <nuno.a.vasques@outlook.com>
2021-12-15 04:18:38 +0000

    Read me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from './Images/Logo.png'
+import logosub from './Images/LogoSubTitle.png'
+import Data from './data.json'
+
+//import linkedin from './Images/linkedin.png'
+
+import { Body, Img, Container, ContLinks, Links, ContSocials, Social, SubTitle, ContEvent, } from './styles'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const isMobile = window.screen.width < 600
+  if (isMobile) {
+    return (
+      <Body>
+        <img src={logo} width="50%" height="50%" alt="" />
+        <Img><img src={logosub} width="60%" height="60%" alt="" /></Img>
+        <Container>
+          <ContSocials>
+            {
+              Data.Social.map(data => {
+                return (
+                  <div key={data.name}>
+                    <Social onClick={() => window.open(data.link, "_self")}><img src={data.path} width={25} height={25} alt={data.name} /></Social>
+                  </div>
+                )
+              })
+            }
+          </ContSocials>
+          <ContLinks>
+            {
+              Data.Links.map(data => {
+                return (
+                  <div key={data.name}>
+                    <Links onClick={() => window.open(data.link, "_self")}>{data.name}</Links>
+                  </div>
+                )
+              })
+            }
+
+          </ContLinks>
+          <SubTitle>Our Events</SubTitle>
+          <ContEvent>
+            {
+              Data.Events.map(data => {
+                return (
+                  <div key={data.name}>
+                    <div onClick={() => window.open(data.link, "_self")}>
+                      <img src={data.path} width="100vw" height="100vh" alt={data.name} />
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </ContEvent>
+        </Container>
+      </Body>
+    );
+  } else {
+    return (
+      <p>Width {'>'} 600</p>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,75 @@
+
+import styled from "styled-components";
+
+export const Body = styled.div`
+    margin:5% 8%;
+    text-align: center; 
+
+`
+export const Img = styled.div`
+    margin-top: -10%;
+    max-height: 150px;
+
+`
+export const Container = styled.div`
+    padding-top:3%;
+    text-align: center; 
+
+`
+export const ContLinks = styled.div`
+    margin-top: -5.5%;
+    text-align: center; 
+    display: grid;
+    grid-gap: 6%;
+    padding: 10% 5%;
+    border-color: #1690C3;
+    border-style: solid;
+    border-radius: 7px;
+    border-width: thin;
+`
+export const ContSocials = styled.div`
+    padding: 0% 23%;
+    background-color: white;
+    z-index: 2;
+    align-items: center;
+    display:inline-flex;
+    gap: 8%;
+    justify-content:space-around;
+    border-color: #1690C3;
+    border-style: solid;
+    border-width: thin;
+    border-radius: 40px;
+`
+export const Social = styled.div`
+    margin-top: 2.5%;
+`
+export const Links = styled.div`
+    padding:2% 0;
+    border-color: #1690C3;
+    border-style: solid;
+    border-width: thin;
+    border-radius: 4px;
+    font-family: Helvetica;
+    color:#1690C3;
+`
+export const SubTitle = styled.div`
+    font-family: Helvetica;
+    color:#1690C3;
+    padding-top:20%;
+    font-size: 150%;
+    font-weight: bolder;
+`
+export const ContEvent = styled.div`
+    margin-top:3%;
+    width: auto;
+    height: auto;
+    display:inline-flex;
+    padding:2%;
+    box-shadow: -5px 10px 20px -10px grey;
+    border-color: #808080;
+    border-style: solid;
+    border-width: thin;
+    border-radius: 7px;
+`
+
+    ;
