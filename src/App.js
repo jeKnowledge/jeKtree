cc487959d0ba3dfd022800a4c800180a2e55bda4
Vasques <nuno.a.vasques@outlook.com>
2021-12-26 21:20:17 +0000

    LinkTree Nova Mockup

--- src/App.js
+++ src/App.js
@@ -1,62 +1,108 @@
-import React from "react";
-import logo from './Images/Logo.png'
-import logosub from './Images/LogoSubTitle.png'
+import React, { useEffect, useState } from "react";
 import Data from './data.json'
-
-//import linkedin from './Images/linkedin.png'
-
-import { Body, Img, Container, ContLinks, Links, ContSocials, Social, SubTitle, ContEvent, } from './styles'
-
+import { Header, ContLinks, Links, SubTitle, ContEvent, EventLogo, EventImg, Footer, Social, ContDots, EventTitle } from './styles'
 
 
 function App() {
 
   const isMobile = window.screen.width < 600
+  const [current, setCurrent] = useState(0);
+  const length = Data.Events.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  const moveSlide = index => {
+    setCurrent(index)
+  }
   if (isMobile) {
     return (
-      <Body>
-        <img src={logo} width="50%" height="50%" alt="" />
-        <Img><img src={logosub} width="60%" height="60%" alt="" /></Img>
-        <Container>
-          <ContSocials>
-            {
-              Data.Social.map(data => {
-                return (
-                  <div key={data.name}>
-                    <Social onClick={() => window.open(data.link, "_self")}><img src={data.path} width={25} height={25} alt={data.name} /></Social>
-                  </div>
-                )
-              })
-            }
-          </ContSocials>
-          <ContLinks>
-            {
-              Data.Links.map(data => {
-                return (
-                  <div key={data.name}>
-                    <Links onClick={() => window.open(data.link, "_self")}>{data.name}</Links>
-                  </div>
-                )
-              })
-            }
+      <div>
+        <Header>
+          <div><img src="Logo.png" width="32%" height="32%" alt="" /></div>
+          <img src="LogoSubTitle.png" width="37%" height="37%" alt="" />
+        </Header>
 
-          </ContLinks>
-          <SubTitle>Our Events</SubTitle>
-          <ContEvent>
-            {
-              Data.Events.map(data => {
-                return (
-                  <div key={data.name}>
-                    <div onClick={() => window.open(data.link, "_self")}>
-                      <img src={data.path} width="100vw" height="100vh" alt={data.name} />
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </ContEvent>
-        </Container>
-      </Body>
+        <ContLinks>
+          {
+
+            Data.Links.map(data => {
+              return (
+                <div key={data.name}>
+                  <Links onClick={() => window.open(data.link, "_self")}>{data.name}</Links>
+                </div>
+              )
+            })
+          }
+        </ContLinks>
+        <SubTitle>Our Events</SubTitle>
+
+        <ContEvent>
+          <EventLogo className='sliderL'>
+            {Data.Events.map((Image, index) => {
+              return (
+                <div
+                  className={index === current ? 'slide activeL' : 'slideL'}
+                  key={index}
+                >
+                  {index === current && (
+                    <img src={Image.pathLogo} alt="" className='imageL' />
+                  )}
+                </div>
+              );
+            })}
+          </EventLogo>
+          <EventImg className='slider'>
+            {Data.Events.map((Image, index) => {
+              return (
+                <div
+                  className={index === current ? 'slide active' : 'slide'}
+                  key={index}
+                >
+                  {index === current && (
+                    <img src={Image.pathImg} alt="" className='image' />
+                  )}
+                </div>
+              );
+            })}
+          </EventImg>
+
+        </ContEvent>
+        <EventTitle>{Data.Events[current].name}</EventTitle>
+        <ContDots>
+          {Array.from({ length: Data.Events.length }).map((item, index) => (
+            <div
+              onClick={() => moveSlide(index)}
+              className={index === current ? "dot active" : "dot"}
+            ></div>
+          ))}
+        </ContDots>
+
+        <Footer>
+          {
+
+            Data.Social.map(data => {
+              return (
+                <div key={data.name}>
+                  <Social onClick={() => window.open(data.link, "_self")}><img src={data.path} alt={data.name} /></Social>
+                </div>
+              )
+            })
+          }
+        </Footer>
+
+
+
+
+
+
+
+
+      </div >
     );
   } else {
     return (
--- src/styles.js
+++ src/styles.js
@@ -1,78 +1,153 @@
 
-import styled from "styled-components";
+import styled from "styled-components"
 
-export const Body = styled.div`
-    margin:5% 8%;
-    text-align: center; 
-    height: auto;
-    width: auto;
-
-`
-export const Img = styled.div`
-    margin-top: -10%;
-    max-height: 150px;
-
-`
-export const Container = styled.div`
-    padding-top:3%;
-    text-align: center; 
-
+export const Header = styled.div`
+    margin: 10% 0;
 `
 export const ContLinks = styled.div`
-    margin-top: -5.5%;
+    margin 0 8%;
+    width:auto;
     text-align: center; 
     display: flex; 
     flex-direction: column; 
-    gap: 2vh;
-    padding: 10% 5%;
-    border-color: #1690C3;
-    border-style: solid;
-    border-radius: 7px;
-    
-    border-width: thin;
-`
-export const ContSocials = styled.div`
-    padding: 0% 23%;
-    background-color: white;
-    z-index: 2;
-    align-items: center;
-    display:inline-flex;
-    gap: 8%;
-    justify-content:space-around;
-    border-color: #1690C3;
-    border-style: solid;
-    border-width: thin;
-    border-radius: 40px;
-`
-export const Social = styled.div`
-    margin-top: 20%;
+
 `
 export const Links = styled.div`
-    padding:2% 0;
-    border-color: #1690C3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow:auto;
+    margin-bottom:2%;
+    border-color: #13234E;
     border-style: solid;
     border-width: thin;
-    border-radius: 4px;
+    border-radius: 13px;
+    background-color:#13234E;
     font-family: 'Suisse-Int-Thin';
-    color:#1690C3;
+    color: #FFFFFF;
+    min-height: 4.75vh;
 `
 export const SubTitle = styled.div`
+    margin-top:8%;
     font-family: 'Suisse-Int-Medium';
-    color:#7CCCED;
-    padding-top:5%;
-    font-size: 150%;
+    color:#FFFFFF;
+    padding:5%;
+    font-size: 120%;
 `
 export const ContEvent = styled.div`
-    margin-top:3%;
-    width: auto;
-    height: auto;
-    display:inline-flex;
-    padding:2%;
-    box-shadow: -5px 10px 20px -10px grey;
-    border-color: #808080;
+    position:relative;
+    width: 90%;
+    background-color:#13234E;
+    margin: 0 auto;
+    border-color: #13234E;
     border-style: solid;
     border-width: thin;
-    border-radius: 7px;
+    border-radius: 13px;
+    
+    
 `
+export const EventLogo = styled.div`
+    position:absolute;
+    left: 2%;
+    bottom: 10%;
+    padding: 2%;
 
+    & .sliderL {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      
+    & .imageL {
+        width: 100%;
+        height: 100%;
+      }
+      
+    & .slideL {
+        opacity: 0;
+        transition-duration: 1s ease;
+      }
+      
+    & .slide.activeL {
+        opacity: 1;
+        transition-duration: 1s;
+        transform: scale(1.08);
+      }
+
+
+`
+export const EventImg = styled.div`
+    & .slider {
+        border-color: #FFFFFF;
+        border-style: solid;
+
+      }
+      
+    & .image {
+        position:relative;
+        max-width: 65vw;
+        max-height: 20vh;
+        border-radius: 10px;
+        padding: 2% 0; 
+        padding-top:3%;
+        padding-left:15%;
+        margin-right:-8%;    
+      }
+      
+    & .right-arrow {
+        position: absolute;
+        top: 50%;
+        right: 32px;
+        font-size: 3rem;
+        color: #000;
+        z-index: 10;
+        cursor: pointer;
+        user-select: none;
+      }
+    & .slide {
+        opacity: 0;
+        transition-duration: 1s ease;
+      }
+      
+    & .slide.active {
+        opacity: 1;
+        transition-duration: 1s;
+        transform: scale(1.0);
+      }
+`
+export const EventTitle = styled.div`
+    margin:5%;
+    font-family: 'Suisse-Int-Thin';
+    color:#FFFFFF;
+    
+`
+export const ContDots = styled.div`
+    display: inline-flex;
+    & .dot{
+        width: 5px;
+        height: 5px;
+        border-radius: 50%;
+        border: 2px solid #FFFFFF;
+        margin: 0 5px;
+        background: #13234E;
+    }
+
+    & .dot.active{
+        background: #FFFFFF;
+    }
+ `
+export const Footer = styled.div`
+    margin-top:10%;
+    background-color: #13234E;
+    display:flex;
+    gap:5%;
+    justify-content: center;
+`
+export const Social = styled.div`
+    padding:25% 0;
+    padding-top:50%;
+    width: 40%;
+    height: 40%;
+
+`
     ;
