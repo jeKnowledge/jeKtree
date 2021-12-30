d75b98c9b72a0c5849191109e0e3776c0694bb93
Vasques <nuno.a.vasques@outlook.com>
2021-12-30 19:40:05 +0000

    Space Image of events

--- src/DesktopStyles.js
+++ src/DesktopStyles.js
@@ -132,7 +132,7 @@ export const EventImg = styled.div`
         border-radius: 10px;
         width: 45vw;
         height: auto;
-        margin-right:-20%;
+        margin-right:-21.5%;
 
            
       }
